d39e36b9f1f6b05059c9071a9705aa6ab54229b9
Milos <bolic.m@mail.ru>
2016-09-16 17:59:26 +0200

    Realized responsive look for mobiles - Index page

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 $(window).scroll(function()
 {
-    if ($(document).scrollTop() > 700)
+    if ($(document).scrollTop() > 800)
     {
-        $('.logo img').addClass('logoSmanji');
+        $('.logo img').addClass('logoSmanji ');
         $('.nav2').addClass('nav2Scroll');
     }
     else
     {
-        $('.logo img').removeClass('logoSmanji');
+        $('.logo img').removeClass('logoSmanji ');
         $('.logo img').addClass('logo');
         $('.nav2').removeClass('nav2Scroll');
     }
 });
+
+
+function resp()
+{
+    var x = document.getElementById("meni2id");
+    if (x.className === "meni2")
+    {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "meni2";
+    }
+}
